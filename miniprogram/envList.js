65763b70f88e6a18ed712922378799bd20c660e6
nhm <3059350051@qq.com>
2023-02-05 16:19:35 +0800

    refactor 小程序代码初始化

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-8gde89ykd7916df3","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\wechat_app\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-8gde89ykd7916df3 --n quickstartFunctions --r --project "C:\Users\nhm\Desktop\wx-music" --report_first --report
