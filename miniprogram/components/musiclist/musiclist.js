aadac40a5e2b76f344b8b067ec57ea1d7d42a1a6
nhm <3059350051@qq.com>
2023-02-07 20:12:16 +0800

    feat 高亮显示选中的歌曲

--- miniprogram/components/musiclist/musiclist.js
+++ miniprogram/components/musiclist/musiclist.js
@@ -11,13 +11,22 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    playingId: -1
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onSelect(event) {
+      const ds = event.currentTarget.dataset
+      const musicid = ds.musicid
+      this.setData({
+        playingId: musicid
+      })
+      wx.navigateTo({
+        url: `../../pages/player/player?musicId=${musicid}`,
+      })
+    }
   }
 })
