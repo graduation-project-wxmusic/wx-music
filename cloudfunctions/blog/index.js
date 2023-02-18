3f4a28ac68c51b47ab992ff2042436ee57889190
nhm <3059350051@qq.com>
2023-02-18 22:19:05 +0800

    feat 博客评论列表

--- cloudfunctions/blog/index.js
+++ cloudfunctions/blog/index.js
@@ -31,5 +31,18 @@ exports.main = async (event, context) => {
     ctx.body = blogList
   })
 
+  app.router('detail', async (ctx, next) => {
+    let blogId = event.blogId
+    const blog = await blogCollection.aggregate().match({
+      _id: blogId
+    }).lookup({
+      from: 'blog-comment',
+      localField: '_id',
+      foreignField: 'blogId',
+      as: 'commentList'
+    }).end()
+    ctx.body = blog
+  })
+
   return app.serve()
 }
