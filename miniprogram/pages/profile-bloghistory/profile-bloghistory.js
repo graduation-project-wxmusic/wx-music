635883e97afbeaa85e086834a431c163dd5a4fd6
nhm <3059350051@qq.com>
2023-02-19 19:57:03 +0800

    feat 博客历史页面

--- miniprogram/pages/profile-bloghistory/profile-bloghistory.js
+++ miniprogram/pages/profile-bloghistory/profile-bloghistory.js
@@ -1,18 +1,22 @@
 // pages/profile-bloghistory/profile-bloghistory.js
+
+const MAX_LIMIT = 10
+const db = wx.cloud.database()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    blogList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this._getListByMiniprogram()
   },
 
   /**
@@ -54,13 +58,39 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
-
+    this._getListByMiniprogram()
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
+  onShareAppMessage(event) {
+    const blog = event.target.dataset.blog
+    return {
+      title: blog.content,
+      path: `/pages/blog-comment/blog-comment?blogId=${blog._id}`
+    }
+  },
 
-  }
+  _getListByMiniprogram() {
+    wx.showLoading({
+      title: '加载中',
+    })
+    db.collection('blog').skip(this.data.blogList.length).limit(MAX_LIMIT).orderBy('createTime', 'desc').get().then((res) => {
+      let _bloglist = res.data
+      for (let i = 0, len = _bloglist.length; i < len; i++) {
+        _bloglist[i].createTime = _bloglist[i].createTime.toString()
+      }
+      this.setData({
+        blogList: this.data.blogList.concat(_bloglist)
+      })
+      wx.hideLoading()
+    })
+  },
+
+  goComment(event) {
+    wx.navigateTo({
+      url: `../blog-comment/blog-comment?blogId=${event.target.dataset.blogid}`,
+    })
+  },
 })
